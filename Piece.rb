63754e8fe11e557d2104c81baf4ac89935aa8440
brett <brett.labash@gmail.com>
2019-03-19 12:22:16 -0700

    starts piece classes

--- Piece.rb
+++ Piece.rb
@@ -1,10 +1,31 @@
 class Piece
   attr_reader :pos
-  def initialize(pos)
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
     @pos = pos
   end
 
   def inspect
     'piece'
   end
+end
+
+
+class Rook < Piece
+  def initialize(color, board, pos)
+    super
+  end
+
+  def symbol
+    if @color == :white
+      return "♖"
+    else
+      return "♜"
+    end
+  end
+
+
+
+
 end
